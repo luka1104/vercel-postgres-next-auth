426aec99988e9eb713551da4caa7c2aaaa1663b7
luka1104 <skyward.luke11@gmail.com>
2023-05-21 19:30:57 +0900

    Commit message: Update options and getServerSession for API auth and gameWorkspace pages

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,7 +3,7 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import GitHubProvider from "next-auth/providers/github"
 import prisma from "lib/prisma"
 
-const options = {
+export const options = {
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID,
--- src/pages/api/gameWorkspace/index.ts
+++ src/pages/api/gameWorkspace/index.ts
@@ -1,10 +1,11 @@
-import { getSession } from "next-auth/react"
+import { getServerSession } from "next-auth/next"
 import prisma from "lib/prisma"
+import { options } from "pages/api/auth/[...nextauth]"
 
 export default async function handle(req, res) {
   const { title, content } = req.body
 
-  const session = await getSession({ req })
+  const session = await getServerSession(req, res, options)
   const result = await prisma.gameWorkspace.create({
     data: {
       title: title,
